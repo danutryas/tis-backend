fa6b846a570b29c1dc9d99de512e3ebd03081c3e
Danu Tryas <Danupristowo09@gmail.com>
2024-01-01 19:43:02 +0700

    remove sync force

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync({ force: true });
+db.sequelize.sync();
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to tis application" });
